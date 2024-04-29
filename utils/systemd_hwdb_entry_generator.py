takaswie/am-config-roms

commit d7a284e8eacd77ab7f02da42873cd8f664ddef3d

add Python 3 script to generate entry for hardware database of systemd
My patchset for udev rules and hardware database was merged to systemd.
 * https://github.com/systemd/systemd/pull/19124

This commit adds Python 3 script to generate entry in the database from
image file of configuration ROM.

utils/systemd_hwdb_entry_generator.py
@@ -1,0 +1,195 @@
+#!/usr/bin/env python3
+
+class SystemdHwdbEntryGenerator:
+    @classmethod
+    def generate(cls, filename, bus_info, root_directory, verbose):
+        node_attrs = cls.__emulate_node_attrs(bus_info, root_directory)
+
+        units_attrs = []
+        if 'units' in node_attrs:
+            for (key_type, key_value) in root_directory:
+                if key_type == 'UNIT':
+                    units_attrs.append(cls.__emulate_unit_attrs(key_value))
+
+        if len(units_attrs) > 0:
+            for i, unit_attrs in enumerate(units_attrs):
+                node_key = cls.__generate_custom_node_key(node_attrs, i)
+                modalias = cls.__generate_unit_modalias(node_attrs, unit_attrs)
+                data = cls.__generate_unit_data(node_attrs, unit_attrs)
+
+                if len(units_attrs) > 1:
+                    print('# generated with {} for unit {}'.format(filename, i))
+                else:
+                    print('# generated with {}'.format(filename))
+                print(node_key)
+                print(modalias)
+                for key, value in data.items():
+                    print('  {}={}'.format(key, value))
+        else:
+            print('This node includes no unit.')
+
+        if verbose:
+            print('')
+            cls.__print_attrs(node_attrs, units_attrs)
+
+    # I decide it for hwdb of systemd. Two types of format are used depending on cases.
+    @classmethod
+    def __generate_custom_node_key(cls, attrs, unit_index):
+        if 'units' in attrs:
+            units = attrs['units'].split(' ')
+            if len(units) == 1:
+                unit = 'units{}'.format(units[unit_index])
+            else:
+                unit = 'units*{}*'.format(units[unit_index])
+        else:
+            unit = ''
+
+        if 'model' in attrs:
+            model = 'mo{}'.format(attrs['model'])
+        else:
+            model = ''
+        return 'ieee1394:node:ven{}{}{}'.format(attrs['vendor'], model, unit)
+
+    # Linux FireWire subsystem decides.
+    @classmethod
+    def __generate_unit_modalias(cls, node_attrs, unit_attrs):
+        ven = 0
+        for attrs in (unit_attrs, node_attrs):
+            if 'vendor' in attrs:
+                ven = int(attrs['vendor'], 16)
+
+        mo = 0
+        for attrs in (unit_attrs, node_attrs):
+            if 'model' in attrs:
+                mo = int(attrs['model'], 16)
+
+        spec = 0
+        if 'specifier_id' in unit_attrs:
+            spec = int(unit_attrs['specifier_id'], 16)
+
+        ver = 0
+        if 'version' in unit_attrs:
+            ver = int(unit_attrs['version'], 16)
+
+        return 'ieee1394:ven{:08X}mo{:08X}sp{:08X}ver{:08X}'.format(ven, mo, spec, ver)
+
+    @classmethod
+    def __generate_unit_data(cls, node_attrs, unit_attrs):
+        iidc_pairs = (
+            ('0x00a02d', '0x000100'),  # IIDC v1.04
+            ('0x00a02d', '0x000101'),  # IIDC v1.20
+            ('0x00a02d', '0x000102'),  # IIDC v1.30, v1.31, v1.32
+            ('0x00a02d', '0x000110'),  # IIDC2 v1.0.0, v1.1.0
+        )
+        pgrey_pairs = (
+            ('0x00b09d', '0x000100'),
+            ('0x00b09d', '0x000101'),
+            ('0x00b09d', '0x000102'),
+        )
+        generic_avc_pairs = (
+            ('0x00a02d', '0x010001'),
+        )
+        vendor_avc_pairs = (
+            ('0x00a02d', '0x014001'),
+        )
+
+        data = {}
+
+        data['ID_VENDOR_FROM_DATABASE'] = '(fill this entry!)'
+        for attrs in (node_attrs, unit_attrs):
+            if 'vendor_name' in attrs and isinstance(attrs['vendor_name'], str):
+                data['ID_VENDOR_FROM_DATABASE'] = attrs['vendor_name']
+
+        data['ID_MODEL_FROM_DATABASE'] = '(fill this entry!)'
+        for attrs in (node_attrs, unit_attrs):
+            if 'model_name' in attrs and isinstance(attrs['model_name'], str):
+                data['ID_MODEL_FROM_DATABASE'] = attrs['model_name']
+
+        if 'specifier_id' in unit_attrs and 'version' in unit_attrs:
+            unit_pair = (unit_attrs['specifier_id'], unit_attrs['version'])
+            if unit_pair in iidc_pairs:
+                data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '1'
+            elif unit_pair in pgrey_pairs:
+                data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '1'
+            elif unit_pair in generic_avc_pairs:
+                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
+            elif unit_pair in vendor_avc_pairs:
+                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
+            else:
+                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
+
+        return data
+
+    @classmethod
+    def __print_attrs(cls, node_attrs, units_attrs):
+        print('Attributes emulated for Linux FireWire subsystem:')
+
+        print('  Node attributes:')
+        for key, value in node_attrs.items():
+            print('    ATTR{{{}}}=="{}"'.format(key, value))
+        for i, unit_attrs in enumerate(units_attrs):
+            print('  Unit {} attributes:'.format(i))
+            for key, value in unit_attrs.items():
+                print('    ATTR{{{}}}=="{}"'.format(key, value))
+
+    @classmethod
+    def __emulate_node_attrs(cls, bus_info, root_directory):
+        attrs = {}
+
+        attrs['guid'] = '0x{:06x}{:012x}'.format(bus_info['node-vendor-id'], bus_info['chip-id'])
+
+        vendor_flag = False
+        model_flag = False
+        units = []
+        for key_type, key_value in root_directory:
+            if key_type == 'VENDOR':
+                attrs['vendor'] = '0x{:06x}'.format(key_value)
+                vendor_flag = True
+            elif key_type == 'MODEL':
+                attrs['model'] = '0x{:06x}'.format(key_value)
+                model_flag = True
+            elif key_type == 'DESCRIPTOR':
+                if vendor_flag:
+                    attrs['vendor_name'] = key_value
+                    vendor_flag = False
+                elif model_flag:
+                    attrs['model_name'] = key_value
+                    model_flag = False
+            elif key_type == 'UNIT':
+                specifier_id = None
+                version = None
+                for (unit_key_type, unit_key_value) in key_value:
+                    if unit_key_type == 'SPECIFIER_ID':
+                        specifier_id = unit_key_value
+                    elif unit_key_type == 'VERSION':
+                        version = unit_key_value
+                if specifier_id is not None and version is not None:
+                    units.append('0x{:06x}:0x{:06x}'.format(specifier_id, version))
+
+        if len(units):
+            attrs['units'] = ' '.join(units)
+
+        return attrs
+
+    @classmethod
+    def __emulate_unit_attrs(cls, entries):
+        attrs = {}
+        model_name_flag = False
+        for (key_type, key_value) in entries:
+            if key_type == 'SPECIFIER_ID':
+                attrs['specifier_id'] = '0x{:06x}'.format(key_value)
+            elif key_type == 'VERSION':
+                attrs['version'] = '0x{:06x}'.format(key_value)
+            elif key_type == 'MODEL':
+                attrs['model'] = '0x{:06x}'.format(key_value)
+                model_name_flag = True
+            elif key_type == 'DESCRIPTOR' and model_name_flag:
+                attrs['model_name'] = key_value
+                model_name_flag = False
+        return attrs

README
@@ -1,6 +1,6 @@
 # Configuration ROM images for node in IEEE 1394 bus
 
-2021/03/28 Takashi Sakamoto
+2021/04/24 Takashi Sakamoto
 
 ## Description
 
@@ -8,6 +8,7 @@
    mainly includes audio and music units.
  * At present, the purpose of the repository is to generate hardware database in systemd project.
   * https://www.freedesktop.org/software/systemd/man/hwdb.html
+  * A Python 3 script (hwdb-entry-generator) is available to generate database entry.
  * The collected images are categorized according to its functionality. At present:
   * audio and music
    * node includes unit relevant to audio signal processing and musical messaging
@@ -16,7 +17,7 @@
   * video
    * node includes unit relevant to video signal processing
   * composite
-   * node includes several types of unit
+   * node includes several units
 
 ## audio_and_music/bebob/
 

hwdb-entry-generator
@@ -1,0 +1,53 @@
+#!/usr/bin/env python3
+
+from sys import argv, exit
+from pathlib import Path
+from struct import pack, unpack
+
+from utils.ieee1212_config_rom_lexer import Ieee1212ConfigRomLexer
+from utils.ieee1212_root_directory_parser import Ieee1212RootDirectoryParser
+from utils.ieee1394_config_rom_parser import Ieee1394ConfigRomParser
+from utils.systemd_hwdb_entry_generator import SystemdHwdbEntryGenerator
+
+if len(argv) < 2:
+    print('hwdb-entry-generator PATH [VERBOSE]')
+    print('  PATH: path to file of image for configuration ROM')
+    print('  VERBOSE: any value for verbose output to print emulated sysfs attribute')
+    exit(1)
+
+path = Path(argv[1])
+verbose = len(argv) > 2
+
+if not path.exists():
+    print('{} not exists'.format(path.name))
+    exit(1)
+
+if path.is_dir():
+    print('{} is not path to file'.format(path.name))
+    exit(1)
+
+raw = []
+with path.open('rb') as f:
+    data = f.read()
+    if len(data) < 4:
+        print('{} has no content'.format(path.name))
+        exit(1)
+
+    # The parser is written for quadlet-aligned big-endian data.
+    endian_check = unpack('>I', data[4:8])[0]
+    if endian_check == 0x34393331:
+        raw = bytearray()
+        for pos in range(0, len(data), 4):
+            val = unpack('<I', data[pos:(pos + 4)])[0]
+            raw.extend(pack('>I', val))
+    else:
+        raw = data
+
+rom = Ieee1212ConfigRomLexer.detect_entries(raw)
+
+bus_info = Ieee1394ConfigRomParser.parse_bus_info(rom['bus-info'])
+
+parser = Ieee1212RootDirectoryParser()
+entries = parser.parse_root_directory('1394', rom['root-directory'])
+
+SystemdHwdbEntryGenerator.generate(path.name, bus_info, entries, verbose)

utils/ieee1212_config_rom_lexer.py
@@ -1,0 +1,100 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+from struct import unpack
+
+
+class EntryType(Enum):
+    IMMEDIATE = 0x00
+    CSR_OFFSET = 0x01
+    LEAF = 0x02
+    DIRECTORY = 0x03
+
+    @classmethod
+    def check_value(cls, value):
+        return value in (item.value for item in cls)
+
+    def __repr__(self):
+        return "'" + self.name + "'"
+
+
+class Ieee1212ConfigRomLexer():
+    @classmethod
+    def detect_entries(cls, raw):
+        entries = {}
+
+        bus_info_length = cls.__detect_bus_info_length(raw)
+        entries['bus-info'] = raw[4:4 + bus_info_length]
+        raw = raw[4 + bus_info_length:]
+
+        entries['root-directory'] = cls.__detect_directory_entries(raw)
+
+        return entries
+
+    @classmethod
+    def __detect_bus_info_length(cls, raw):
+        meta = unpack('>I', raw[:4])[0]
+        bus_info_quadlet_count = (meta & 0xff000000) >> 24
+        # crc_quadlet_count = (meta & 0x00ff0000) >> 16
+        # crc = meta & 0x0000ffff
+        return bus_info_quadlet_count * 4
+
+    @classmethod
+    def __detect_immediate(cls, key, value, raw):
+        return value
+
+    @classmethod
+    def __detect_csr_offset(cls, key, value, raw):
+        return 0xfffff0000000 + value * 4
+
+    @classmethod
+    def __detect_leaf_or_directory_length(cls, raw):
+        meta = unpack('>I', raw[:4])[0]
+        quadlet_count = (meta & 0xffff0000) >> 16
+        # crc = meta & 0x0000ffff
+        return quadlet_count * 4
+
+    @classmethod
+    def __detect_leaf(cls, key, value, raw):
+        offset = value * 4
+        raw = raw[offset:]
+        length = cls.__detect_leaf_or_directory_length(raw)
+        return raw[4:4 + length]
+
+    @classmethod
+    def __detect_directory(cls, key, value, raw):
+        raw = raw[value * 4:]
+        return cls.__detect_directory_entries(raw)
+
+    @classmethod
+    def __detect_directory_entries(cls, raw):
+        # Table 7 - Directory entry types
+        type_handles = {
+            EntryType.IMMEDIATE: cls.__detect_immediate,
+            EntryType.CSR_OFFSET: cls.__detect_csr_offset,
+            EntryType.LEAF: cls.__detect_leaf,
+            EntryType.DIRECTORY: cls.__detect_directory,
+        }
+        entries = []
+
+        length = cls.__detect_leaf_or_directory_length(raw)
+        raw = raw[4:]
+
+        while length > 0:
+            meta = unpack('>I', raw[:4])[0]
+            key_type_id = (meta & 0xc0000000) >> 30
+            key_id = (meta & 0x3f000000) >> 24
+            entry_value = meta & 0x00ffffff
+
+            if not EntryType.check_value(key_type_id):
+                raise ValueError('Type {0} is not defined.'.format(key_type_id))
+            key_type = EntryType(key_type_id)
+            handle = type_handles[key_type]
+
+            entry = [(key_id, key_type), handle(key_id, entry_value, raw)]
+            entries.append(entry)
+
+            raw = raw[4:]
+            length -= 4
+
+        return entries

utils/ieee1394_config_rom_parser.py
@@ -1,0 +1,86 @@
+#!/usr/bin/env python3
+
+from struct import unpack
+
+
+class Ieee1394ConfigRomParser():
+    __BUS_NAME = 0x31333934
+
+    # added by IEEE 1394:1995
+    __BUS_CAPS_1995 = {
+        'irmc': (0x80000000, 31),  # Isochronous resource manager capable.
+        'cmc': (0x40000000, 30),  # cycle master capable.
+        'isc': (0x20000000, 29),  # isochronous capable.
+        'bmc': (0x10000000, 28),  # bus namager capable.
+        'cyc-clk-acc': (0x00ff0000, 16),  # cycle clock accuracy.
+        'max_rec': (0x0000f000, 12),  # maximum data record size.
+    }
+
+    # added by IEEE 1394a:2000
+    __BUS_CAPS_2000 = {
+        'pmc': (0x08000000, 28),  # power manager capable.
+        'gen': (0x000000c0, 6),  # generation.
+        'link-spd': (0x00000007, 0),  # link speed.
+    }
+
+    # added by IEEE 1394:2008
+    __BUS_CAPS_2008 = {
+        'adj': (0x04000000, 27),  # compliant to IEEE 1394.1:2004.
+    }
+
+    @classmethod
+    def parse_bus_info(cls, raw):
+        meta = unpack('>I', raw[:4])[0]
+        if meta != cls.__BUS_NAME:
+            raise ValueError('The bus_name field mismatch')
+
+        bus_info = {}
+        meta = unpack('>I', raw[4:8])[0]
+
+        for caps in (cls.__BUS_CAPS_1995, cls.__BUS_CAPS_2000, cls.__BUS_CAPS_2008):
+            for key, (mask, shift) in caps.items():
+                bus_info[key] = (meta & mask) >> shift
+
+        meta = unpack('>I', raw[8:12])[0]
+        bus_info['node-vendor-id'] = (meta & 0xffffff00) >> 8
+        bus_info['chip-id'] = ((meta & 0x000000ff) << 32) | unpack('>I', raw[12:16])[0]
+
+        return bus_info
+
+    # IEEE 1394:1995 refers to ISO/IEC 13213:1994 (ANSI/IEEE Std 1212:1994).
+    __NODE_CAPABILITIES = {
+        'misc': {
+            'spt': (0x008000, 15),  # The SPLIT_TIMEOUT register is implemented.
+            'ms': (0x004000, 14),  # The messages-passing registers are implemented.
+            'int': (0x002000, 13),  # The INTERRUPT_TARGET and INTERRUPT_MASK registers are implemented.
+        },
+        'testing': {
+            'ext': (0x001000, 12),  # The ARGUMENT registers are implemented.
+            'bas': (0x000800, 11),  # Node implements TEST_START&TEST_STATUS registers and testing state.
+        },
+        'addressing': {
+            'prv': (0x000400, 10),  # The node implements the private space.
+            '64': (0x000200, 9),  # The node uses 64-bit aaddressing (otherwise 32-bit addressing).
+            'fix': (0x000100, 8),  # The node uses the fixed addressing scheme (otherwise extended addressing).
+        },
+        'state': {
+            'lst': (0x000080, 7),  # The STATE_BITS.lost bit is implemented.
+            'drq': (0x000040, 6),  # The STATE_BITS.dreq bit is implemented.
+            'elo': (0x000020, 4),  # The STATE_BITS.elog bit and the ERROR_LOG registers are implementd.
+            'atn': (0x000008, 3),  # The STATE_BITS.atn bit is implemented.
+            'off': (0x000004, 2),  # The STATE_BITS.off bit is implemented.
+            'ded': (0x000002, 1),  # The node supports the dead state.
+            'init': (0x000001, 0),  # The node supports the initializing state.
+        },
+    }
+
+    @classmethod
+    def parse_node_caps(cls, val):
+        node_caps = {}
+        for category, caps in cls.__NODE_CAPABILITIES.items():
+            if category not in node_caps:
+                node_caps[category] = {}
+            for key, (mask, shift) in caps.items():
+                node_caps[category][key] = (val & mask) >> shift
+
+        return node_caps

commit bf623bfd43df1791b587e4e366af8e3b28836a3b

utils: fix tag name for devices to handle MIDI messages
The tag should be IEEE1394_UNIT_FUNCTION_MIDI, instead of
IEEE1394_UNIT_FUNCTION_MUSIC.

Fixes: d7a284e8eacd ("add Python 3 script to generate entry for hardware database of systemd")

utils/systemd_hwdb_entry_generator.py
@@ -112,15 +112,15 @@ class SystemdHwdbEntryGenerator:
             elif unit_pair in pgrey_pairs:
                 data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '1'
             elif unit_pair in generic_avc_pairs:
-                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_MIDI'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
             elif unit_pair in vendor_avc_pairs:
-                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_MIDI'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
             else:
-                data['IEEE1394_UNIT_FUNCTION_MUSIC'] = '(1 if supported else remove)'
+                data['IEEE1394_UNIT_FUNCTION_MIDI'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_AUDIO'] = '(1 if supported else remove)'
                 data['IEEE1394_UNIT_FUNCTION_VIDEO'] = '(1 if supported else remove)'
 